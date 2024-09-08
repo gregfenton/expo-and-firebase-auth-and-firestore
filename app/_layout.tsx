eae2834295226b94b50d5891fabbc42af49441c9
Greg Fenton <greg.fenton@gmail.com>
2024-09-08 14:02:28 -0400

    doc: add comment for why we need app/(app)/....

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Slot } from "expo-router";
 
+// NOTE: needed to move the Firebase and Auth initialization to
+//       a lower level (app/(app)/_layout.tsx) so that Expo Router
+//       can initialize completely.  Otherwise trying to redirect
+//       the user to either the landing page or the welcome page
+//       fails with 
 export default function RootLayout() {
   return <Slot />;
 }
